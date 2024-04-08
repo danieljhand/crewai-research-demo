8d9cb5227bf81bdfcf439ecbf22bc2e92b901733
Daniel J Hand <daniel.hand@bnc.oxon.org>
2024-04-08 12:31:53 +0800

    Experimented with the properties of the research agent and research task. Results are a significant improvement when used with a custom model based on the openhermes:7b-mistral-v2.5-fp16 llm.

--- research-crew.py
+++ research-crew.py
@@ -17,10 +17,10 @@ llm = ChatOpenAI(
 
 # SerperDevTool provides an API interface to Google search. This is the only tool we'll
 # provide to our AI agents.
-search_tool = SerperDevTool()
+#search_tool = SerperDevTool(n_results=3)
 
 # Alternatively we can use DuckDuckGoSearch
-# search_tool = DuckDuckGoSearchRun()
+search_tool = DuckDuckGoSearchRun()
 
 # Create a senior researcher agent to explore and share knowledge on a given topic. Enabling memory 
 # (to remember past interactions) and verbose mode to assist with debugging and understand what it's doing.
@@ -29,11 +29,27 @@ researcher = Agent(
   goal='Uncover groundbreaking technologies in {topic}',
   verbose=True,
   memory=True,
+  max_rpm=12, # No limit on requests per minute
+  max_iter=12,
   backstory=(
-    "You are a world renowned senior science and technology researcher respected for the quality and reliability of your research."
-    "You are also an excellent collaborator often working with a team of writers that create engaging and informative articles based on your research."
+    "You are a world renowned senior science and technology researcher"
+    "You search the internet for articles on {topic} future trends"
+    "and provide sub topics and URLs to writers to base articles on"
   ),
   tools=[search_tool],
+  llm=llm,
+  allow_delegation=False
+)
+
+editor = Agent(
+  role='Senior Editor',
+  goal='Review and provide feedback on how to improve an article written on {topic}',
+  verbose=True,
+  memory=True,
+  backstory=(
+      "You are an editor for a prestigious academic journal."
+  ),
+  tools=[],
   llm=llm,
   allow_delegation=False
 )
@@ -42,7 +58,7 @@ researcher = Agent(
 # and verbose mode to assist with debugging and understand what it's doing.
 writer = Agent(
   role='Writer',
-  goal='Narrate compelling tech stories about {topic}',
+  goal='Narrate compelling tech stories around {topic}',
   verbose=True,
   memory=True,
   backstory=(
@@ -59,16 +75,30 @@ writer = Agent(
 # and restrict its access to only the search_tool.
 research_task = Task(
   description=(
-    "Identify the next big trend in {topic}."
-    "Focus on identifying pros and cons and the overall narrative."
-    "Your final report should clearly articulate the key points"
-    "its market opportunities and potential risks."
+    "Identify future trends around {topic}. "
+    "Limit the number of trends to at most three areas. "
+    "Focus on identifying the pros and cons of each trends, the market opportunity and risks. "
+    "Create a briefing document for a writer to continue your research. "
+    "Clearly articulate the key points and the narrative you want in the final report. "
   ),
-  expected_output="A comprehensive 3 paragraph report on the latest {topic} trends.",
+  expected_output="A three to five paragraph briefing document on the latest trends around {topic} formatted in markdown ",
   tools=[search_tool],
   agent=researcher,
   async_execution=False,
-  output_file='research-note.txt'
+  output_file='research-notes.md'
+)
+
+
+# Create a new task to review the report and provide clear guidance for improvement.
+review_task = Task(
+  description=(
+      "A new research paper has been submitted that explores future developments in {topic}. Your role is to analyze the paper and provide feedback to the author(s).  What steps do you take to ensure the research is sound, the writing is clear and concise, and the paper is suitable for publication in your journal?"
+  ),
+  expected_output="A comprehensive 5 paragraph report highlighting the main areas of improvement and suggestions on how to improve",
+  tools=[],
+  agent=editor,
+  async_execution=False,
+  output_file='editor-note.txt'
 )
 
 # Create a new task to perform the writing. We associate the task with the writer agent
@@ -91,11 +121,13 @@ write_task = Task(
 # We now define a crew consisting of two agents - researcher and writer - and provide it with an
 # ordered list of tasks to complete sequentially.
 crew = Crew(
-  agents=[researcher, writer],
-  tasks=[research_task, write_task],
+  # agents=[researcher, writer, editor],
+  # tasks=[research_task, write_task, review_task, write_task],
+  agents=[researcher],
+  tasks=[research_task],
   process=Process.sequential,  # Optional: Sequential task execution is default
   memory=True,
-  verbose=9,
+  verbose=1,
   cache=True,
   max_rpm=10,
   share_crew=False
